5787b33113b2bb7586f9961bd783af8249c55db6
Vitor Nunes <stecks10@gmail.com>
2021-05-09 23:02:07 -0300

    Estilização da autenticação

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
-function Signin() {
-  return <h1>Login</h1>
+export default function Signin() {
+  return (
+    <>
+      <img src={logo} alt="Gobarber" />
+
+      <form >
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
+  );
 }
 
-export default Signin;
+
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
-function SignUp() {
-  return <div />;
+export default function SignUp() {
+  return (
+    <>
+      <img src={logo} alt="Gobarber" />
+
+      <form >
+        <input placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
-
-export default SignUp;
