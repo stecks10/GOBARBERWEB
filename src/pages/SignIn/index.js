0d899d572b1aaaedf88245a10a33df46670a45d2
Vitor Nunes <stecks10@gmail.com>
2021-05-09 23:41:05 -0300

    Validações

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
 import logo from '~/assets/logo.svg';
 
-export default function Signin() {
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatôrio'),
+  password: Yup.string().required("A senha é obrigatória"),
+});
+
+export default function SignIn() {
   function handleSubmit(data) {
     console.tron.log(data);
   }
@@ -12,7 +21,7 @@ export default function Signin() {
     <>
       <img src={logo} alt="Gobarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input
           name="password"
