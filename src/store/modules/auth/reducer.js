258893f58068604c1c9a9c1523c9a1a4847560b5
Vitor Nunes <stecks10@gmail.com>
2021-05-13 12:43:59 -0300

    Logout da aplicação

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -13,7 +13,6 @@ export default function auth(state = INITIAL_STATE, action) {
         draft.loading = true;
         break;
       }
-
       case '@auth/SIGN_IN_SUCCESS': {
         draft.token = action.payload.token;
         draft.signed = true;
@@ -22,6 +21,11 @@ export default function auth(state = INITIAL_STATE, action) {
       }
       case '@auth/SIGN_FAILURE': {
         draft.loading = false;
+        break;
+      }
+      case '@auth/SIGN_OUT': {
+        draft.token = null;
+        draft.signed = false;
         break;
       }
       default:
--- src/store/modules/user/reducer.js
+++ src/store/modules/user/reducer.js
@@ -1,7 +1,8 @@
+
 import produce from 'immer';
 
 const INITIAL_STATE = {
-  profile: null,
+  profile: null
 };
 
 export default function user(state = INITIAL_STATE, action) {
@@ -13,6 +14,10 @@ export default function user(state = INITIAL_STATE, action) {
       }
       case '@user/UPDATE_PROFILE_SUCCESS': {
         draft.profile = action.payload.profile;
+        break;
+      }
+      case '@auth/SIGN_OUT': {
+        draft.profile = null;
         break;
       }
       default:
