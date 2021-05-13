681fd8b17e94e325e09083405dd4cd28980437e3
Vitor Nunes <stecks10@gmail.com>
2021-05-12 22:41:03 -0300

    Atualizando perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,29 +1,38 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { updateProfileRequest } from '../../store/modules/user/actions';
 
 import { Container } from './styles';
 
 export default function Profile() {
+  const dispatch = useDispatch();
   const profile = useSelector(state => state.user.profile);
 
   function handleSubmit(data) {
-    console.tron.log(data);
-
+    dispatch(updateProfileRequest(data));
   }
 
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
-        <Input name="name" placeholder="Nome completo" />
-        <Input name="name" type="email" placeholder="Seu endereço de e-mail" />
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de email" />
 
         <hr />
 
-        <Input type="password" name="oldPassword" placeholder="Sua senha atual" />
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
         <Input type="password" name="password" placeholder="Nova senha" />
-        <Input type="password" name="confirmPassword" placeholder="Confirmação de senha" />
-
+        <Input
+          type="password"
+          name="ConfirmPassword"
+          placeholder="Confirmação de senha"
+        />
 
         <button type="submit">Atualizar perfil</button>
       </Form>
